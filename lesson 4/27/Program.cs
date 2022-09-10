0b3357b8e7198e4cb70c21472dcc1dcbd6f53934
yashinBoris98 <111410571+yashinBoris98@users.noreply.github.com>
2022-09-10 11:30:42 +0300

    lesson 3

---
+++ lesson 4/27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+string num = Console.ReadLine();
+int sum = 0;
+foreach (char item in num)
+{
+    // string w = item.ToString();
+    // int n = Convert.ToInt32(w);
+    // sum+=n;
+
+    sum+=Convert.ToInt32(item.ToString());
+}
+Console.WriteLine(sum);
