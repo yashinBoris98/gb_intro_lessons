c04a9e3c1321d8294999a8a9e5c3fa1bc0f4f645
yashinBoris98 <111410571+yashinBoris98@users.noreply.github.com>
2022-09-24 14:05:13 +0300

    54 56

--- lesson 8/54/Program.cs
+++ lesson 8/54/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+
+// 1 2 4 7
+
+// 2 3 5 9
+
+// 2 4 4 8
+
+int[][] CreateArrayOfArrays(int dim1, int dim2){
+
+    Random rand = new Random{};
+    int[][] arr = new int[dim1][];
+    for (int i = 0; i < dim1; i++)
+    {
+        arr[i]=new int[dim2];
+    }
+
+    for (int i = 0; i < dim1; i++)
+    {
+        for (int k = 0; k < dim2; k++)
+        {
+            arr[i][k] = rand.Next(1,999);
+        }
+    }
+    return arr;
+}
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[][] arrayOfArrays = CreateArrayOfArrays(m,n);
+for (int i = 0; i < m; i++)
+{
+    Array.Sort(arrayOfArrays[i]);
+    Console.WriteLine("Start of new array");
+    foreach (var item in arrayOfArrays[i])
+    {
+        Console.WriteLine(item);
+    }
+}
--- lesson 8/56/Program.cs
+++ lesson 8/56/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[][] CreateArrayOfArrays(int dim1, int dim2){
+
+    Random rand = new Random{};
+    int[][] arr = new int[dim1][];
+    for (int i = 0; i < dim1; i++)
+    {
+        arr[i]=new int[dim2];
+    }
+
+    for (int i = 0; i < dim1; i++)
+    {
+        for (int k = 0; k < dim2; k++)
+        {
+            arr[i][k] = rand.Next(1,999);
+        }
+    }
+    return arr;
+}
+
+int AddUpArrMembers(int[] arr){
+    int sum = 0;
+    foreach (var item in arr)
+    {
+        sum+=item;
+    }
+    return sum;
+}
+
+int FindMin(int[] arr){
+    int min = arr[0];
+    foreach (var item in arr)
+    {
+        if(item<min) min=item;
+    }
+    return min;
+}
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[][] arrayOfArrays = CreateArrayOfArrays(m,n);
+int[] sumOfArrays = new int[m];
+for (int i = 0; i < m; i++)
+{
+    int sum = AddUpArrMembers(arrayOfArrays[i]);
+    Console.WriteLine("sum of array is equal: "+sum);
+    sumOfArrays[i]=sum;
+}
+Console.WriteLine("Minimal sum of array is equal: "+FindMin(sumOfArrays));
