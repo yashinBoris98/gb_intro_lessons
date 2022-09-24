4d425bc43e4c9560f7c6758812459fe20de426c6
yashinBoris98 <111410571+yashinBoris98@users.noreply.github.com>
2022-09-24 14:38:40 +0300

    62

--- lesson 8/62/Program.cs
+++ lesson 8/62/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Заполните спирально массив 4 на 4.
+
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+
+// 12 13 14 5
+
+// 11 16 15 6
+
+// 10 9 8 7
+
+int[,] CreateArray(){
+    int[,] arr = new int[4,4];
+    Random rand = new Random();
+    for (int i = 0; i < 4; i++)
+    {
+        for (int k = 0; k < 4; k++)
+        {
+            arr[i,k] = rand.Next(1,99);
+        }
+    }
+    return arr;
+}
+
+int[,] arr = CreateArray();
+for (int i = 0; i < 4; i++)
+{
+    for (int k = 0; k < 4; k++)
+    {
+        Console.Write(arr[i,k]+" ");
+    }
+    Console.WriteLine("");
+}
