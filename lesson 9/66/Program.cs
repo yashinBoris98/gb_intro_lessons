0a8b9700bf1e8af6b7286a35497680e6e4a4ac23
yashinBoris98 <111410571+yashinBoris98@users.noreply.github.com>
2022-10-01 11:32:29 +0300

    lesson 9

---
+++ lesson 9/66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int m =Convert.ToInt32(Console.ReadLine());
+int n =Convert.ToInt32(Console.ReadLine());
+int sum=0;
+
+for (int i = m; i <= n; i++){
+    sum+=i;
+}
+Console.WriteLine(sum);
---
+++ lesson 9/68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+uint m = Convert.ToUInt32(Console.ReadLine());
+uint n = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine(A(m,n));
